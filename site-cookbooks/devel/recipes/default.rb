a7adba59efb5fc80a36394c074ce4c580d2750e5
supercaracal <supercaracal@yahoo.co.jp>
2015-02-13 19:41:00 +0900

    add dot files and change locale, timezone

--- site-cookbooks/devel/recipes/default.rb
+++ site-cookbooks/devel/recipes/default.rb
@@ -11,10 +11,44 @@ package "git" do
   action :install
 end
 
+package "ncurses-term" do
+  action :install
+end
+
+package "language-pack-ja" do
+  action :install
+end
+
+bash "update-locale" do
+  code 'sudo update-locale LANG=ja_JP.UTF-8'
+end
+
+link "/etc/localtime" do
+  to "/usr/share/zoneinfo/Japan"
+end
+
 package "tmux" do
   action :install
 end
 
 package "curl" do
   action :install
+end
+
+template "/home/vagrant/.inputrc" do
+  source ".inputrc.erb"
+  owner "vagrant"
+  group "vagrant"
+end
+
+template "/home/vagrant/.vimrc" do
+  source ".vimrc.erb"
+  owner "vagrant"
+  group "vagrant"
+end
+
+template "/home/vagrant/.tmux.conf" do
+  source ".tmux.conf.erb"
+  owner "vagrant"
+  group "vagrant"
 end
