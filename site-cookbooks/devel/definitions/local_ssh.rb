91df44dcb263758dc64f7ed77d0a38131e9ad82e
supercaracal <supercaracal@yahoo.co.jp>
2015-03-28 11:23:36 +0900

    ssh keepalive

--- site-cookbooks/devel/definitions/local_ssh.rb
+++ site-cookbooks/devel/definitions/local_ssh.rb
@@ -14,4 +14,11 @@ define :local_ssh, user: 'root', home: '/tmp', ssh_keys: [] do
     content params[:ssh_keys].join("\n")
     action :create
   end
+
+  file "#{params[:home]}/.ssh/config" do
+    owner params[:user]
+    group params[:user]
+    content 'ServerAliveInterval 15'
+    action :create
+  end
 end
