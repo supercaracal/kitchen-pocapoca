a0ec8a1befbbed3e6c8a838be679e30eafd97e8b
supercaracal <supercaracal@yahoo.co.jp>
2015-11-23 01:04:48 +0900

    undefined method

--- site-cookbooks/devel/definitions/source.rb
+++ site-cookbooks/devel/definitions/source.rb
@@ -7,7 +7,7 @@ define :source, user: 'root', files: [], destination: '/tmp/.bashrc', command: '
       code <<-EOS
         touch #{params[:destination]}
         echo '' >> #{params[:destination]}
-        echo '# added by configuration tools #{Time.zone.now}' >> #{params[:destination]}
+        echo '# added by configuration tools #{Time.now}' >> #{params[:destination]}
         echo '#{params[:command]} "#{file}"' >> #{params[:destination]}
       EOS
     end
