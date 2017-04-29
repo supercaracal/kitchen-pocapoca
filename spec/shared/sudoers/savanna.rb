210734ea2223e1187f9e34a91acacee50946bafd
supercaracal <supercaracal@yahoo.co.jp>
2017-04-30 00:03:49 +0900

    fix sudoers check serverspec

--- spec/shared/sudoers/savanna.rb
+++ spec/shared/sudoers/savanna.rb
@@ -1,8 +1,5 @@
-shared_examples 'sudoers::savanna' do |user_name|
-  describe file("/etc/sudoers.d/91-#{user_name}") do
-    it { should be_file }
-    it { should be_owned_by('root') }
-    it { should be_grouped_into('root') }
-    it { should be_mode 440 }
+shared_examples 'sudoers::savanna' do
+  describe command('sudo echo hogehoge') do
+    its(:stdout) { should contain('hogehoge') }
   end
 end
