8da8f1a5f0698166fa0f94678585c1b555961ac3
Taishi Kasuga <proxy0721@gmail.com>
2018-01-05 12:29:28 +0900

    Fix serverspec

--- spec/shared/python/init.rb
+++ spec/shared/python/init.rb
@@ -1,4 +1,4 @@
-shared_examples 'ruby::init' do |version: '3.6.4'|
+shared_examples 'python::init' do |version: '3.6.4'|
   describe command('/home/pocapoca/.pyenv/shims/python --version') do
     its(:stdout) { should contain(/Python #{version}/) }
   end
